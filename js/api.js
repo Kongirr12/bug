351f11e47401c6beb666d07e24bff3aea6bbd162
Kongirr12 <kongirr.12@gmail.com>
2026-07-03 21:06:31 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -3,6 +3,8 @@ const CONFIG = {
     // Replace this URL with the Web App URL generated when you deploy Code.gs in Google Apps Script
     API_URL: 'https://script.google.com/macros/s/AKfycbyJi8hhxx-L_3xqokw_ceLcPPl5KPHMEU-Cqt1aIVjfIsGUZvRKsv6jitRvZGV_eFWx/exec'
 };
+
+
 /**
  * Make an API call to the Google Apps Script backend.
  * @param {string} action The function name to call in Apps Script
